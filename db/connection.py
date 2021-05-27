827750126a6af0abc0dcf323e78fb45a238f0490
Anna Tychenko <aniatychenko@gmail.com>
2021-05-27 14:30:34 +0300

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,3 +1,12 @@
 import pymongo
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_url = os.getenv('DB_URL')
+db_name = os.getenv('DB_NAME')
+
+client = pymongo.MongoClient(db_url)
+db = client[db_name]
